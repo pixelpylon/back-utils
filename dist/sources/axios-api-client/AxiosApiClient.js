ec409de7b23ed639aac0437c07949fc1d2d5afe1
Ivan Kosolapov <polzuka@gmail.com>
2023-03-02 13:38:08 +0500

    Fixed AxiosApiClient typing

--- dist/sources/axios-api-client/AxiosApiClient.js
+++ dist/sources/axios-api-client/AxiosApiClient.js
@@ -8,14 +8,29 @@ const makeApiRequest = async (execute) => {
     }
 }
 
-const AxiosApiClient = (axiosInstance) => {
-    return {
-        instance: axiosInstance,
-        get: (url, config) => makeApiRequest(() => axiosInstance.get(url, config)),
-        delete: (url, config) => makeApiRequest(() => axiosInstance.delete(url, config)),
-        put: (url, data, config) => makeApiRequest(() => axiosInstance.put(url, data, config)),
-        post: (url, data, config) => makeApiRequest(() => axiosInstance.post(url, data, config)),
-        patch: (url, data, config) => makeApiRequest(() => axiosInstance.patch(url, data, config)),
+class AxiosApiClient {
+    constructor(axiosInstance) {
+        this.axiosInstance = axiosInstance
+    }
+
+    get(url, config) {
+        return makeApiRequest(() => this.axiosInstance.get(url, config))
+    }
+
+    delete(url, config) {
+        return makeApiRequest(() => this.axiosInstance.delete(url, config))
+    }
+
+    put(url, data, config) {
+        return makeApiRequest(() => this.axiosInstance.put(url, data, config))
+    }
+
+    post(url, data, config) {
+        return makeApiRequest(() => this.axiosInstance.post(url, data, config))
+    }
+
+    patch(url, data, config) {
+        return makeApiRequest(() => this.axiosInstance.patch(url, data, config))
     }
 }
 
