6c758c802d4caed782a0337180416792fe195944
Ivan Kosolapov <polzuka@gmail.com>
2023-03-02 14:06:01 +0500

    Fixed AxiosApiClient

--- dist/declares/axios-api-client/AxiosApiClient.d.ts
+++ dist/declares/axios-api-client/AxiosApiClient.d.ts
@@ -2,6 +2,7 @@ import {AxiosInstance, AxiosRequestConfig, AxiosResponse} from "axios"
 
 export class AxiosApiClient {
     public readonly axiosInstance: AxiosInstance
+    constructor(axiosInstance: AxiosInstance)
     get<T = unknown, R = AxiosResponse<T>, D = any>(url: string, config?: AxiosRequestConfig<D>): Promise<R>
     delete<T = unknown, R = AxiosResponse<T>, D = any>(url: string, config?: AxiosRequestConfig<D>): Promise<R>
     post<T = unknown, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: AxiosRequestConfig<D>): Promise<R>
