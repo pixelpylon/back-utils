1c09558b46cb5543b39e18056ca172052842dc5b
Ivan Kosolapov <polzuka@gmail.com>
2023-08-25 18:22:40 +0500

    Refactoring

--- dist/declares/index.d.ts
+++ dist/declares/index.d.ts
@@ -2,7 +2,7 @@ export {AxiosApiClient} from "./axios-api-client/AxiosApiClient"
 export {AbstractRpcRequestHandler, RpcClientConfigRecord} from "./rpc/AbstractRpcRequestHandler"
 export {RpcClient} from "./rpc/RpcClient"
 export {AbstractRequestHandler} from './request-handling/AbstractRequestHandler'
-export {handleRequest} from "./request-handling/handleRequest"
+export {handleRequest, entityRouter} from "./request-handling"
 export {authenticateUser, SESSION_COOKIE} from './auth/authenticateUser'
 export {CONFIG_PROPERTY_TYPES, IRemoteConfigTemplate, loadRemoteConfig, PropertyDescriptor, getLimitAndOffset} from './utils'
 export * as validators from './validators'
